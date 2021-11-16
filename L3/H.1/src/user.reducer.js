1579dacbcd62eaf3cf249c69d4c025064aabae5f
BlvckMagik <dandol777@gmail.com>
2021-11-16 13:43:11 +0200

    less

--- L3/H.1/src/user.reducer.js
+++ L3/H.1/src/user.reducer.js
@@ -3,17 +3,11 @@ import { ADD_USER, DELETE_USER } from './user.actions.js';
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case ADD_USER: {
-      return {
-        ...state,
-        user: action.payload,
-      };
+      return action.payload;
     }
 
     case DELETE_USER: {
-      return {
-        ...state,
-        user: null,
-      };
+      return null;
     }
 
     default:
