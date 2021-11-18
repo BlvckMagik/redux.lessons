47240e4ce612247d725b2758d021c69625c629d6
BlvckMagik <dandol777@gmail.com>
2021-11-18 15:11:40 +0200

    less

---
+++ L4/C.1/src/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import counterReduser from './counter.reducer.js';
+
+const store = createStore(
+  counterReduser,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
